a17208a6884df7f35d51a96850384c42ec411697
Sobhan Mohammadpour <sobhanmohamadpour@gmail.com>
2023-10-10 18:10:23 +0200

    add: dog breed page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sobhan.dogbreeds"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sobhan.dogbreeds"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
